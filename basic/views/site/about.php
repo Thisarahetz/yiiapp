baa3568ad74804bff5042894ff9fb2537c1d81fa
Thisara Hettikankanam <43540833+Thisarahetz@users.noreply.github.com>
2024-04-16 21:19:09 +0530

    Update about.php

--- basic/views/site/about.php
+++ basic/views/site/about.php
@@ -4,12 +4,16 @@
    $this->title = 'About';
    $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
+<div class = "site-about">
    <h1><?= Html::encode($this->title) ?></h1>
    <p>
       This is the About page. You may modify the following file to customize its content:
    </p>
-   <?= $this->render("_part1") ?>
-   <?= $this->render("_part2") ?>
+   <p>
+      <b>email:</b> <?= $email ?>
+   </p>
+   <p>
+      <b>phone:</b> <?= $phone ?>
+   </p>
    <code><?= __FILE__ ?></code>
 </div>
