df233766817183be095baf138e34c6594cd28e0e
Thisara Hettikankanam <43540833+Thisarahetz@users.noreply.github.com>
2024-04-20 22:03:04 +0530

    Update SiteController.php

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -617,14 +617,60 @@ class SiteController extends Controller
                 // ->one();
                 // var_dump($user);
 
-                $users = (new \yii\db\Query())
-                ->select(['id', 'name', 'email'])
-                ->from('user')
-                ->limit(5)
-                ->offset(5)
-                ->all();
-                var_dump($users);
+                // $users = (new \yii\db\Query())
+                // ->select(['id', 'name', 'email'])
+                // ->from('user')
+                // ->limit(5)
+                // ->offset(5)
+                // ->all();
+                // var_dump($users);
+
+                 // return a single user whose ID is 1
+                // SELECT * FROM `user` WHERE `id` = 1
+                // $user = User::find()
+                // ->where(['id' => 1])
+                // ->one();
+                // var_dump($user);
+                // // return the number of users
+                // // SELECT COUNT(*) FROM `user`
+                // $users = User::find()
+                // ->count();
+                // var_dump($users);
+                // // return all users and order them by their IDs
+                // // SELECT * FROM `user` ORDER BY `id`
+                // $users = User::find()
+                // ->orderBy('id')
+                // ->all();
+                // var_dump($users);
+
+                // $user = MyUser::findOne(1);
+                // var_dump($user);
+                // // returns customers whose ID is 1,2,3, or 4
+                // // SELECT * FROM `user` WHERE `id` IN (1,2,3,4)
+                // $users = MyUser::findAll([1, 2, 3, 4]);
+                // var_dump($users);
+                // // returns a user whose ID is 5
+                // // SELECT * FROM `user` WHERE `id` = 5
+                // $user = MyUser::findOne([
+                //     'id' => 5
+                // ]);
+                // var_dump($user);
+
+                // insert a new row of data
+                // $user = new User();
+                // $user->name = 'MyCustomUser2';
+                // $user->email = 'mycustomuser@gmail.com';
+                // $user->save();
+                // var_dump($user->attributes);
+                
+                // // update an existing row of data
+                // $user = User::findOne(['name' => 'MyCustomUser2']);
+                // $user->email = 'newemail@gmail.com';
+                // $user->save();
+                // var_dump($user->attributes);
+                $user = User::findOne(2);
+                if($user->delete()) {
+                    echo "deleted";
+                } 
         }
-
-
 }
