2a9b4a3c4e9320b35cbb587e4758cf90c14753df
Thisara Hettikankanam <43540833+Thisarahetz@users.noreply.github.com>
2024-04-20 10:47:20 +0530

    Yii - Validation

--- basic/models/RegistrationForm.php
+++ basic/models/RegistrationForm.php
@@ -4,21 +4,36 @@
     use yii\base\Model;
     class RegistrationForm extends Model {
         public $username;
-        public $password;
-        public $email;
-        public $subscriptions;
-        public $photos;
-        /**
-         * @return array customized attribute labels
-         */
+      public $password;
+      public $email;
+      public $country;
+      public $city;
+      public $phone;
+
+        public function rules() {
+             return [
+                [['username', 'password', 'email', 'country', 'city', 'phone'], 'required'],
+                ['email', 'email'],
+                ['phone', 'number'],
+                ['username', 'required', 'message' => 'Please enter your name'],
+                ['password', 'required', 'message' => 'Please enter your password'],
+                ['email', 'required', 'message' => 'Please enter your email'],
+                ['country', 'required', 'message' => 'Please enter your country'],
+                ['city', 'required', 'message' => 'Please enter your city'],
+                ['phone', 'required', 'message' => 'Please enter your phone'],
+             ];
+        }
+
         public function attributeLabels() {
             return [
                 'username' => 'Username',
                 'password' => 'Password',
                 'email' => 'Email',
-                'subscriptions' => 'Subscriptions',
-                'photos' => 'Photos',
+                'country' => 'Country',
+                'city' => 'City',
+                'phone' => 'Phone',
             ];
         }
+        
     }
 ?>
--- basic/views/site/registration.php
+++ basic/views/site/registration.php
@@ -2,19 +2,20 @@
    use yii\bootstrap\ActiveForm;
    use yii\helpers\Html;
 ?>
+
 <div class = "row">
    <div class = "col-lg-5">
       <?php $form = ActiveForm::begin(['id' => 'registration-form']); ?>
-      <?= $form->field($model, 'username') ?>
-      <?= $form->field($model, 'password')->passwordInput() ?>
-      <?= $form->field($model, 'email')->input('email') ?>
-      <?= $form->field($model, 'photos[]')->fileInput(['multiple'=>'multiple']) ?>
-      <?= $form->field($model, 'subscriptions[]')->checkboxList(['a' => 'Item A',
-         'b' => 'Item B', 'c' => 'Item C']) ?>
-      <div class = "form-group">
-         <?= Html::submitButton('Submit', ['class' => 'btn btn-primary',
-            'name' => 'registration-button']) ?>
-      </div>
+         <?= $form->field($model, 'username') ?>
+         <?= $form->field($model, 'password')->passwordInput() ?>
+         <?= $form->field($model, 'email')->input('email') ?>
+         <?= $form->field($model, 'country') ?>
+         <?= $form->field($model, 'city') ?>
+         <?= $form->field($model, 'phone') ?>
+         <div class = "form-group">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary',
+               'name' => 'registration-button']) ?>
+         </div>
       <?php ActiveForm::end(); ?>
    </div>
 </div>
