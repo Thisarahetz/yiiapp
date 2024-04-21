847af1c491e33fc89edf1f3899628bad63449674
Thisara Hettikankanam <43540833+Thisarahetz@users.noreply.github.com>
2024-04-21 12:10:09 +0530

    Generating Controller

--- basic/controllers/CustomController.php
+++ basic/controllers/CustomController.php
@@ -1,9 +1,26 @@
 <?php
-   namespace app\modules\hello\controllers;
+   namespace app\controllers;
    use yii\web\Controller;
+   use app\models\User;
    class CustomController extends Controller {
-      public function actionGreet() {
-         return $this->render('greet');
+      // public function actionGreet() {
+      //    return $this->render('greet');
+      // }
+      public function actionHello() {
+         return $this->render('hello');
+      }
+      public function actionIndex() {
+         return $this->render('index');
+      }
+      public function actionWorld() {
+         return $this->render('world');
+      }
+
+      public function actionView() {
+         $model = new User();
+         return $this->render('customview', [
+            'model' => $model,
+         ]);
       }
    }
 ?>
---
+++ basic/views/custom/customview.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\User $model */
+/** @var ActiveForm $form */
+?>
+<div class="customview">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'name') ?>
+        <?= $form->field($model, 'email') ?>
+    
+        <div class="form-group">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        </div>
+    <?php ActiveForm::end(); ?>
+
+</div><!-- customview -->
